c568f67c7dc1a080b480406294eb94feb69530d5
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2025-09-04 07:08:02 +0700

    refactor: reorganize imports and enhance createLink functionality in TRPC routes

--- packages/data-ops/src/queries/links.ts
+++ packages/data-ops/src/queries/links.ts
@@ -1,22 +1,23 @@
-import { getDb } from '@/db/database';
-import { links } from '@/drizzle-out/schema';
-import { CreateLinkSchemaType } from '@/zod/links';
-import { eq } from 'drizzle-orm';
-import { nanoid } from 'nanoid';
+import { getDb } from "@/db/database";
+import { links } from "@/drizzle-out/schema";
+import { CreateLinkSchemaType, LinkSchemaType } from "@/zod/links";
+import { nanoid } from "nanoid";
 
-export async function createLink(data: CreateLinkSchemaType & { accountId: string }) {
-    const db = getDb();
+export async function createLink(
+  data: CreateLinkSchemaType & { accountId: string }
+): Promise<LinkSchemaType["linkId"]> {
+  const db = getDb();
 
-    const id = nanoid(10);
+  const id = nanoid(10);
 
-    const values = {
-        linkId: id,
-        accountId: data.accountId,
-        name: data.name,
-        destinations: JSON.stringify(data.destinations),
-    };
-    
-    const [result] = await db.insert(links).values(values).returning({ linkId: links.linkId });
+  const values = {
+    linkId: id,
+    accountId: data.accountId,
+    name: data.name,
+    destinations: JSON.stringify(data.destinations),
+  };
 
-    return result.linkId;
+  const [result] = await db.insert(links).values(values).returning();
+
+  return result.linkId;
 }
