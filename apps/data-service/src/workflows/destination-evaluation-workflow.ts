8127bab2ab25bd8e2324ba10dfb91eb3a0a28dd7
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2025-09-17 10:47:46 +0700

    feat: add example parameters for DestinationStatusEvaluationParams in destination-evaluation-workflow.ts

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -2,6 +2,12 @@ import { DestinationStatusEvaluationParams, Env } from '@/bindings';
 import { collectDestinationInfo } from '@/helpers/browser-render';
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 
+const exampleParams: DestinationStatusEvaluationParams = {
+    "linkId": "EqZQf0WQ8Q",
+    "destinationUrl": "https://akbarsahata.id",
+    "accountId": "1234567890",
+}
+
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
     async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
 
